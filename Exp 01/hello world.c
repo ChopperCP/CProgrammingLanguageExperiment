d57f4a4f9fa3f1f4a9d03c8b71ecdb49a04f6590
ChopperCP <568624486@qq.com>
2021-03-03 18:15:21 +0800

    First experiment: Hello world

---
+++ Exp 01/hello world.c
@@ -0,0 +1,8 @@
+#include <stdio.h> //导入头文件，以使用printf()
+
+// argc 和 argv 虽然本实验没必要，但为了保持良好的编程习惯就加上了
+int main(int argc, char *argv[])
+{
+    printf("%s", "Hello world!"); // Hello world!
+    return 0;                     // POSIX规范
+}
--- test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char *argv[])
-{
-    printf("%s", "Hello world!");
-    return 0;
-}
